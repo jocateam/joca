936cc83cab5bdade689e92f48ff6b035582e96d0
nicolas balao <nicolasbalao.pro@gmail.com>
2023-02-06 17:07:49 +0000

    :hammer: refactor(frontend): Switch to angular for the frontend

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'joca-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend';
+}
